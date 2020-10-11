d3a4f34dae5812f668d5fd51788f3677a00a4e31
samuell324 <chistov_artem@mail.ru>
2020-10-11 23:15:45 +0300

    split the drawing into two custompaint, create MainWidget class

--- lib/smileWidget.dart
+++ lib/smileWidget.dart
@@ -4,27 +4,47 @@ import 'dart:math' as Math;
 class SmileyPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    final radius = Math.min(size.width, size.height) / 4;
+    final radius = Math.min(size.width, size.height) / 2.5;
     final center = Offset(size.width / 2, size.height / 2);
-
     // Body
     final paint = Paint()..color = Colors.yellow;
     canvas.drawCircle(center, radius, paint);
-
-    // Mouth
-    final smilePaint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 10;
-    canvas.drawArc(Rect.fromCircle(center: center, radius: radius / 2), 0,
-        Math.pi, false, smilePaint);
-
     // Eyes
     canvas.drawCircle(
         Offset(center.dx - radius / 2, center.dy - radius / 2), 10, Paint());
     canvas.drawCircle(
         Offset(center.dx + radius / 2, center.dy - radius / 2), 10, Paint());
   }
-
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class Smile extends CustomPainter {
+  //Mouth
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = Math.min(size.width, size.height) / 2.5;
+    drawMouth() {
+      final smilePaint = Paint()
+        ..style = PaintingStyle.fill
+        ..strokeWidth = 10;
+      canvas.drawArc(Rect.fromCircle(center: center, radius: radius / 2), 0,
+          Math.pi, false, smilePaint);
+    }
+    drawMouth();
+  }
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class MainWidget extends StatefulWidget {
+  @override
+  _MainWidgetState createState() => _MainWidgetState();
+}
+
+class _MainWidgetState extends State<MainWidget> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
