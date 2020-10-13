a0128f547dad2e28d91738cf042f4cb31ed89067
samuell324 <chistov_artem@mail.ru>
2020-10-13 13:47:10 +0300

    move SmileyPainter to separate class

--- lib/smileWidget.dart
+++ lib/smileWidget.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as Math;
-
-class SmileyPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final radius = Math.min(size.width, size.height) / 2.5;
-    final center = Offset(size.width / 2, size.height / 2);
-    // Body
-    final paint = Paint()..color = Colors.yellow;
-    canvas.drawCircle(center, radius, paint);
-    // Eyes
-    canvas.drawCircle(
-        Offset(center.dx - radius / 2, center.dy - radius / 2), 10, Paint());
-    canvas.drawCircle(
-        Offset(center.dx + radius / 2, center.dy - radius / 2), 10, Paint());
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
-}
+import 'smileBody.dart';
 
 class Smile extends CustomPainter {
   //Mouth
