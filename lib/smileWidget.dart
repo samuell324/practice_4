61d0cb0211f5f19a0045f2676d4ba66e9dfddf7d
samuell324 <chistov_artem@mail.ru>
2020-10-13 13:51:05 +0300

    move Smile class to separate class smileMouth

--- lib/smileWidget.dart
+++ lib/smileWidget.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as Math;
 import 'smileBody.dart';
-
-class Smile extends CustomPainter {
-  //Mouth
-  void paint(Canvas canvas, Size size) {
-    final center = Offset(size.width / 2, size.height / 2);
-    final radius = Math.min(size.width, size.height) / 2.5;
-    drawMouth() {
-      final smilePaint = Paint()
-        ..style = PaintingStyle.fill
-        ..strokeWidth = 10;
-      canvas.drawArc(Rect.fromCircle(center: center, radius: radius / 2), 0,
-          Math.pi, false, smilePaint);
-    }
-
-    drawMouth();
-  }
-
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
-}
+import 'smileMouth.dart';
 
 class MainWidget extends StatefulWidget {
   @override
