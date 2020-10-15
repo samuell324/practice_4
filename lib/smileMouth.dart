d0eb8bb541fdc2ab879ecf471daa837300e0d8d6
samuell324 <chistov_artem@mail.ru>
2020-10-15 08:58:51 +0300

    change smile eyes from circle to oval

--- lib/smileMouth.dart
+++ lib/smileMouth.dart
@@ -4,12 +4,11 @@ class Smile extends CustomPainter {
   //Mouth
   void paint(Canvas canvas, Size size) {
     final center = Offset(size.width / 2, size.height / 2);
-    final radius = Math.min(size.width, size.height) / 2.5;
     drawMouth() {
       final smilePaint = Paint()
         ..style = PaintingStyle.fill
         ..strokeWidth = 10;
-      canvas.drawArc(Rect.fromCircle(center: center, radius: radius / 2), 0,
+      canvas.drawArc(Rect.fromCircle(center: center, radius: 50), 0,
           Math.pi, false, smilePaint);
     }
 
