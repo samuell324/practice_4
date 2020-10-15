23687a804cd5cdcc3df752e9d12d43aa13c4a9f8
samuell324 <chistov_artem@mail.ru>
2020-10-15 14:27:25 +0300

    rename CustomWidgetApp to app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:practice4/main.dart';
-import 'package:practice4/customWidgetApp.dart';
+import 'package:practice4/app.dart';
 
 
 void main() {
